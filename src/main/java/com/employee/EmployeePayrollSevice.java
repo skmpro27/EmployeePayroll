da13b6505967d1ef6c214dc16f940f75a72578da
unknown <kumar.skm03@gmail.com>
2021-03-25 21:48:10 +0530

    added method to retrive data from table in database

--- src/main/java/com/employee/EmployeePayrollSevice.java
+++ src/main/java/com/employee/EmployeePayrollSevice.java
@@ -1,11 +1,29 @@
 package com.employee;
 
+import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.sql.Statement;
 
 class EmployeePayrollService {
     DatabaseConnection con = new DatabaseConnection();
 
     public String connect() throws ClassNotFoundException, SQLException {
         return con.connectionToDatabase();
+    }
+
+    public int dataInTable() throws SQLException, ClassNotFoundException {
+        connect();
+        Statement statement = con.connection.createStatement();
+        ResultSet resultSet = statement.executeQuery( "SELECT * FROM employee_payroll");
+        int count = 0;
+        while (resultSet.next()) {
+            count++;
+            System.out.println(resultSet.getString(1) +
+                               " Name: " + resultSet.getString(2) +
+                               " Date " + resultSet.getString(3) +
+                               " Salary " + resultSet.getString(4));
+        }
+        con.connection.close();
+        return count;
     }
 }
--- src/test/java/com/employee/EmployeePayrollTest.java
+++ src/test/java/com/employee/EmployeePayrollTest.java
@@ -1,20 +1,36 @@
 package com.employee;
 
 import org.junit.Assert;
+import org.junit.Before;
 import org.junit.Test;
 
 import java.sql.SQLException;
 
 public class EmployeePayrollTest {
+    EmployeePayrollService employeePayroll;
+
+    @Before
+    public void setUp() throws Exception {
+        employeePayroll = new EmployeePayrollService();
+    }
+
     @Test
-    public void givenDatabase() {
-        EmployeePayrollService employeePayroll = new EmployeePayrollService();
+    public void givenDatabaseSetupConnectionReturnConnected() {
         try {
             String result = employeePayroll.connect();
             Assert.assertEquals("Connected..", result);
         } catch (ClassNotFoundException | SQLException e) {
             e.printStackTrace();
         }
+    }
 
+    @Test
+    public void givenDatabaseRetrieveDataInTable() {
+        try {
+            int result = employeePayroll.dataInTable();
+            Assert.assertEquals(4, result);
+        } catch (ClassNotFoundException | SQLException e) {
+            e.printStackTrace();
+        }
     }
 }
